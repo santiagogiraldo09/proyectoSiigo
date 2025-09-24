71cdd9729293ad153dd43e5a2ba8a475f7b59728
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2025-09-24 11:41:40 -0500

    Update appSiigoEliminandoPrimeras7Filas.py

--- appSiigoEliminandoPrimeras7Filas.py
+++ appSiigoEliminandoPrimeras7Filas.py
@@ -481,11 +481,11 @@ if 'conectado' not in st.session_state:
 # Inputs para las rutas de los archivos
 ruta_fija = st.text_input(
     "Ruta completa del archivo FIJO en SharePoint",
-    "Documentos Compartidos/Reportes/ArchivoFijo.xlsx"
+    "Documentos/01 Archivos Area Administrativa/TRM.xlsx"
 )
 ruta_carpeta_mensual = st.text_input(
     "Ruta de la CARPETA que contiene los archivos mensuales",
-    "Documentos Compartidos/ReportesMensuales"
+    "Documentos/Ventas con ciudad 2025"
 )
 
 if st.button("Conectar y Verificar Archivos"):
